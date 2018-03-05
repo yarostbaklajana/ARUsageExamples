3230a7485449b25f11fdd72fadfae039fb7d969c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-05 08:20:28 +0000

    Deploy yarostbaklajana/ARUsageExamples to github.com/yarostbaklajana/ARUsageExamples.git:gh-pages

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./app",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Welcome to AR',
+    filename: '../index.html'
+  })]
+};
