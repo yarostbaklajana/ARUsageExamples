542fa3f9f38381df95447219ac34dcea78a63f95
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2018-03-12 23:05:41 +0200

    Fixed config and attribute

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist/"
+    publicPath: "dist/"
   },
   module: {
     rules: [
@@ -38,7 +38,7 @@ module.exports = {
         use: [ {
           loader: 'html-loader',
           options: {
-            attrs: [':src', ':url']
+            attrs: [':src', ':url', ':patternUrl']
           }
         },]
       },
