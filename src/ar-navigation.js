dfcad709cacdf1a82b611612e38efddf004a3e83
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2018-03-17 14:09:50 +0200

    Added component, fixed conflicts

--- src/ar-navigation.js
+++ src/ar-navigation.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Entity} from 'aframe-react';
 import {Box, Sphere, Cylinder, Plane, Sky, Text, Scene, Camera, Entity as AREntity} from 'react-aframe-ar';
+import './components';
+import { cameraChangeHandler } from './components';
+
+console.log('Aframe', AFRAME);
 
 class MarkerCamera extends AREntity {
     constructor (props) {
@@ -18,15 +22,30 @@ const containerStyle = {
     height: 100 + '%'
 }
 
+const EmployeeMarker = ({props}) => {
+    return (
+        <Text></Text>
+    );
+};
+
 export const ARNavigation = ({match}) => (
     <div style={containerStyle}>
         <Scene embedded arjs>
-            <Box position="-1 0.5 -10" rotation="0 45 0" color="#4CC3D9" shadow />
-            <Sphere position="0 1.25 -10" radius="1.25" color="#EF2D5E" shadow />
-            <Cylinder position="1 0.75 -10" radius="0.5" height="1.5" color="#FFC65D" shadow />
-            <Plane position="0 0 -10" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow />
-            <MarkerCamera preset="hiro"></MarkerCamera>
-            <Camera look-controls/>
+            <Box 
+                position="-1 0.5 -3" 
+                rotation="0 45 0" 
+                color="#4CC3D9" 
+                opacity="0.5" 
+                shadow 
+                gps-place="longitude: 37.469820; latitude: 55.153653"
+            />
+            <Camera 
+                componentchange={ (ev) => { console.log('CHanged '); cameraChangeHandler(ev); }} 
+                look-controls 
+                user-height="1.6" 
+                gps-position 
+                compass-rotation
+            />
         </Scene>
-        </div>
+    </div>
 );
